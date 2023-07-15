43c4aa84308da0c0422035b6dfdf15b07af82ef6
Ilia Nikolaev <ilyateletski@gmail.com>
2023-07-15 23:08:30 +0700

    Add section our-routes

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,16 @@ $(document).ready(function(){
     dots: true,
     autoplay: true,
     fade: true,
-    
-    
+  });
+});
+
+$(document).ready(function(){
+  $('.our-routes__carusel').slick({
+    arrows: false,
+    dots: true,
+    // autoplay: true,
+    slidesToShow: 1,
+    variableWidth: true,
+
   });
 });
