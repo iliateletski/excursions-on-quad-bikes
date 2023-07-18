bc4a795718be9ba4081d92e887b1810251a0c46c
Ilia Nikolaev <ilyateletski@gmail.com>
2023-07-18 21:31:06 +0700

    Add sections photo_gallery and route selection

--- app/js/main.js
+++ app/js/main.js
@@ -8,12 +8,39 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-  $('.our-routes__carusel').slick({
+  $('.photo-gallery__carusel').slick({
     arrows: false,
     dots: true,
+    slidesToShow: 3,
     // autoplay: true,
-    slidesToShow: 1,
-    variableWidth: true,
-
+    dotsClass: 'carousel-dots',
   });
 });
+
+$(function () {
+  const elements = $(".our-routes__carusel") 
+  const classes = ['.carusel-ltr', '.carusel-rtl']
+  for(let i = 0; i < elements.length; i++) {
+    if(i % 2 !== 0) {
+      $(elements[i]).attr('dir', 'rtl')
+      $(elements[i]).addClass('carusel-rtl')
+    } else {
+      $(elements[i]).addClass('carusel-ltr')
+    }
+  }
+  console.log(elements)
+
+  for(let i = 0; i < classes.length; i++) {
+    console.log(classes[i])
+    $(classes[i]).slick({
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      slidesToShow: 1,
+      variableWidth: true,
+      rtl: classes[i] === '.carusel-rtl' ? true : false,
+      dotsClass: 'carousel-dots'
+    });
+  }
+})
+
